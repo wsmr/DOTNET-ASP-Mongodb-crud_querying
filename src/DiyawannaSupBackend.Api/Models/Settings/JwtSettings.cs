55d218685c6ae66b97844ccf623402200d3e0dcd
Sahan <w.s.m.rathnasekara@gmail.com>
2025-06-24 06:35:06 +0530

    Create JwtSettings.cs

---
+++ src/DiyawannaSupBackend.Api/Models/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace DiyawannaSupBackend.Api.Models.Settings
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; }
+        public int ExpirationMinutes { get; set; }
+    }
+}
+v
