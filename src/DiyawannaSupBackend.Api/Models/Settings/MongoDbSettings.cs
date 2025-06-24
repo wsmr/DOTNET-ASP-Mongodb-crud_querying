9a4d05519bd10edfd26e26645cccd7b8a2b8d318
Sahan <w.s.m.rathnasekara@gmail.com>
2025-06-24 06:34:42 +0530

    Create MongoDbSettings.cs

---
+++ src/DiyawannaSupBackend.Api/Models/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace DiyawannaSupBackend.Api.Models.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
